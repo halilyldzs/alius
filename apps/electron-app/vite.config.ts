2eb29efc03c006a022afe6620c7f51821bb71aa2
halilyldz <halilibrahim.yildiz002@gmail.com>
2025-04-20 22:31:18 +0300

    chore: update dependencies and integrate Tailwind CSS into Electron app

--- apps/electron-app/vite.config.ts
+++ apps/electron-app/vite.config.ts
@@ -1,10 +1,11 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
